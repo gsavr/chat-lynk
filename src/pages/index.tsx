baaa5fd4d4e9b58fb4beee83825ea0f434f8a7a6
giorgio <giorgiosavron@me.com>
2023-06-21 15:51:30 -0400

    landing page animation

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,6 +6,7 @@ import { authOptions } from "./api/auth/[...nextauth]";
 import { Header } from "@/components/header/header";
 import logo from "../images/logo-c.svg";
 import Image from "next/image";
+import { motion } from "framer-motion";
 import { Footer } from "@/components/footer/footer";
 
 const Home: NextPage = () => {
@@ -26,26 +27,61 @@ const Home: NextPage = () => {
           <div className="flex h-full flex-col items-center justify-center space-y-2.5 bg-white/40 backdrop-blur-lg backdrop-brightness-75">
             {status === "loading" ? null : (
               <>
-                <Image height={100} src={logo} alt="logo" priority />
-                <div className="text-lg font-medium text-black/50 md:text-2xl lg:text-3xl">
+                <motion.div
+                  initial={{ scale: 0.1 }}
+                  animate={{ scale: 1 }}
+                  transition={{
+                    duration: 0.4,
+                  }}
+                >
+                  <motion.div
+                    initial={{ scale: 5, y: 100 }}
+                    animate={{ scale: 1, y: 0 }}
+                    transition={{
+                      duration: 0.8,
+                      delay: 1,
+                    }}
+                  >
+                    <Image height={100} src={logo} alt="logo" priority />
+                  </motion.div>
+                </motion.div>
+                <motion.div
+                  initial={{ opacity: 0, scale: 0.3 }}
+                  animate={{ opacity: 1, scale: 1 }}
+                  transition={{
+                    duration: 0.6,
+                    delay: 1.2,
+                  }}
+                  className="text-lg font-medium text-black/50 md:text-2xl lg:text-3xl"
+                >
                   Welcome to LynkChat
-                </div>
-                <div>
-                  <button
-                    onClick={() => signIn("Auht0")}
-                    className="text-base text-black/50 transition hover:text-[#D7A761]/100"
-                  >
-                    Join us
-                  </button>
-                </div>
-                <div>
-                  <button
-                    onClick={() => signIn("Auht0")}
-                    className="inline-flex h-12 items-center rounded border border-transparent bg-white/50 px-6 text-lg font-medium text-black/60"
-                  >
-                    Sign in
-                  </button>
-                </div>
+                </motion.div>
+                <motion.div
+                  initial={{ opacity: 0, y: 400 }}
+                  animate={{ opacity: 1, y: 0 }}
+                  transition={{
+                    duration: 0.8,
+                    delay: 1,
+                  }}
+                  className="flex flex-col items-center gap-2"
+                >
+                  <div>
+                    <button
+                      onClick={() => signIn("Auht0")}
+                      className="text-base text-black/50 transition hover:text-[#D7A761]/100"
+                    >
+                      Join us
+                    </button>
+                  </div>
+                  <div>
+                    <button
+                      onClick={() => signIn("Auht0")}
+                      className="inline-flex h-12 items-center rounded border border-transparent bg-white/50 px-6 text-lg font-medium text-black/60"
+                    >
+                      Sign in
+                    </button>
+                  </div>
+                </motion.div>
               </>
             )}
           </div>
